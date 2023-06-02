ee56df3d8794e68040dabba4cf28b4dedd214f07
Robert <robert.mabry86@gmail.com>
2023-06-01 22:45:53 -0500

    created, tested, and indexed datasbase of api calls

---
+++ api/tacoData.js
@@ -0,0 +1,94 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getTacos = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/taco.json?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const createTaco = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/taco.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const setcode = { firebaseKey: data.name };
+      fetch(`${endpoint}/taco/${setcode.firebaseKey}.json`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(setcode),
+      }).then(resolve);
+    })
+    .catch(reject);
+});
+
+const getSingleTaco = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/taco/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const deleteSingleTaco = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/taco/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateTaco = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/taco/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+export {
+  getTacos,
+  createTaco,
+  getSingleTaco,
+  deleteSingleTaco,
+  updateTaco,
+};
