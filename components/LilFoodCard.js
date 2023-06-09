40511e6e207146bb5f351c9418fd9cc984f8bc24
Robert <robert.mabry86@gmail.com>
2023-06-08 19:25:28 -0500

    made lil food card

---
+++ components/LilFoodCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import { deleteSingleCritic } from '../api/criticData';
+
+function BigFoodCard({ foodObj, onUpdate }) {
+  const deleteThisCard = () => {
+    // check foodObj.name
+    if (window.confirm(`Delete ${foodObj.criticName}?`)) {
+      deleteSingleCritic(foodObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Body>
+        <Card.Title>{foodObj.taco_id.ShopName}</Card.Title>
+        <p className="card-text bold">{foodObj.criticName}</p>
+        <p className="card-text bold">{foodObj.desc}</p>
+        <p className="card-text bold">{foodObj.rating}</p>
+        <Link href={`/board/${foodObj.firebaseKey}`} passHref>
+          <Button variant="success" className="m-2">VIEW</Button>
+        </Link>
+        <Link href={`/board/edit/${foodObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisCard} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+BigFoodCard.propTypes = {
+  foodObj: PropTypes.shape({
+    criticName: PropTypes.string,
+    desc: PropTypes.string,
+    taco_id: PropTypes.string,
+    createdDate: PropTypes.instanceOf(Date),
+    rating: PropTypes.number,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default BigFoodCard;
