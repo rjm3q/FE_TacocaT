c36427d4a766e5e90ea249e2180250d7cf66efa5
Robert <robert.mabry86@gmail.com>
2023-06-10 13:30:14 -0500

    updated app.js and tacos folder files

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,20 @@
-/* eslint-disable react/prop-types */
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import NoAuth from '../components/NoAuth'; // TODO: COMMENT OUT FOR AUTH
 import '../styles/globals.css';
-import { AuthProvider } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
-import ViewDirectorBasedOnUserAuthStatus from '../utils/ViewDirector'; // TODO: COMMENT IN FOR AUTH
+import { AuthProvider } from '../utils/context/authContext';
+import ViewDirectorBasedOnUserAuthStatus from '../utils/ViewDirector';
 
+// eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      {/* TODO: Delete NoAuth component above and comment in code below for authentication */}
-      <AuthProvider>
-        <ViewDirectorBasedOnUserAuthStatus
-          // if status is pending === loading
-          // if status is logged in === view app
-          // if status is logged out === sign in page
-          component={Component}
-          pageProps={pageProps}
-        />
-      </AuthProvider>
-    </>
+    <AuthProvider> {/* gives children components access to user and auth methods */}
+      <ViewDirectorBasedOnUserAuthStatus
+        // if status is pending === loading
+        // if status is logged in === view app
+        // if status is logged out === sign in page
+        component={Component}
+        pageProps={pageProps}
+      />
+    </AuthProvider>
   );
 }
-
 export default MyApp;
