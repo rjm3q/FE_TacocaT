d186f1cd32866f23e2122719b1047d05a4a16bea
Robert <robert.mabry86@gmail.com>
2023-06-17 12:33:13 -0500

    modified styling for the index pages, changed values in critics firebaseKey page

--- pages/tacos/tacoIndex.js
+++ pages/tacos/tacoIndex.js
@@ -20,12 +20,6 @@ function Home() {
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
     >
 
       {/* <h1>Hello {user.displayName}! </h1>
